940dcdb9b82afece50fcd841f0787422355922f7
Jkenyut <satrianursaputro06@gmail.com>
2023-06-02 14:26:55 +0700

    push

---
+++ entity/actor.go
@@ -0,0 +1,14 @@
+package entity
+
+import "time"
+
+type Actor struct {
+	ID        uint64    `gorm:"column:id;primaryKey;autoIncrement"`
+	Username  string    `gorm:"column:username;unique;size:255"`
+	Password  string    `gorm:"column:password;size:255"`
+	RoleID    uint32    `gorm:"column:role_id;default:2"`
+	Verified  string    `gorm:"column:verified;type:enum('true','false');default:'false'"`
+	Active    string    `gorm:"column:active;type:enum('true','false');default:'false'"`
+	CreatedAt time.Time `gorm:"column:created_at;type:timestamp;default:current_timestamp"`
+	UpdatedAt time.Time `gorm:"column:updated_at;type:timestamp;default:current_timestamp;autoUpdateTime"`
+}
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
-package crm_service
+package main
+
+import (
+	"crm_service/modules/actor"
+	db2 "crm_service/utils/db"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 
+	_ = godotenv.Load()
+	db := db2.GormMysql()
+	router := gin.New()
+
+	actorHandler := actor.NewRouter(db)
+	actorHandler.Handle(router)
+
+	errRouter := router.Run(":8081")
+	if errRouter != nil {
+		fmt.Println("error running server", errRouter)
+		return
+	}
 }
